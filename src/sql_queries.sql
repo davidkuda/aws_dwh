eb8010c92eb4484cfd3cea1806ab413c17f4d66a
David Kuda <david.kuda@pm.me>
2021-05-30 15:36:47 +0200

    Update "create table" queries with additional params

--- src/sql_queries.sql
+++ src/sql_queries.sql
@@ -1,45 +1,45 @@
 CREATE TABLE IF NOT EXISTS songplays (
-	songplay_id INTEGER SORTKEY DISTKEY,
-	start_time TIMESTAMP,
-	user_id INTEGER,
-	level VARCHAR(20),
-	song_id INTEGER,
-	artist_id INTEGER,
-	session_id INTEGER,
-	location VARCHAR(50),
-	user_agent VARCHAR
+	songplay_id INTEGER PRIMARY KEY NOT NULL SORTKEY DISTKEY,
+	start_time TIMESTAMP NOT NULL REFERENCES time(start_time),
+	user_id INTEGER NOT NULL REFERENCES users(user_id),
+	level VARCHAR(20) NOT NULL,
+	song_id INTEGER NOT NULL REFERENCES songs(song_id),
+	artist_id INTEGER NOT NULL REFERENCES artists(artist_id),
+	session_id INTEGER NOT NULL,
+	location VARCHAR(50) NOT NULL,
+	user_agent VARCHAR NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS  users (
-	user_id INTEGER SORTKEY,
-	first_name VARCHAR(50),
-	last_name VARCHAR(50),
-	gender VARCHAR(20),
-	level VARCHAR(20)
+	user_id INTEGER PRIMARY KEY NOT NULL SORTKEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	gender VARCHAR(20) NOT NULL,
+	level VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS songs (
-	song_id INTEGER SORTKEY,
-	title VARCHAR(50),
-	artist_id INTEGER,
-	year SMALLINT,
-	duration SMALLINT
+	song_id INTEGER PRIMARY KEY NOT NULL SORTKEY,
+	title VARCHAR(50) NOT NULL,
+	artist_id INTEGER NOT NULL,
+	year SMALLINT NOT NULL,
+	duration SMALLINT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS artists (
-	artist_id INTEGER SORTKEY,
-	name VARCHAR(50),
-	location VARCHAR(50),
-	latitude DECIMAL,
-	longitude DECIMAL
+	artist_id INTEGER PRIMARY KEY NOT NULL SORTKEY,
+	name VARCHAR(50) NOT NULL,
+	location VARCHAR(50) NOT NULL,
+	latitude DECIMAL NOT NULL,
+	longitude DECIMAL NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS time (
-	start_time TIMESTAMP,
-	hour SMALLINT,
-	day SMALLINT,
-	week SMALLINT,
-	month SMALLINT,
-	year SMALLINT,
-	weekday SMALLINT
+	start_time TIMESTAMP PRIMARY KEY NOT NULL,
+	hour SMALLINT NOT NULL,
+	day SMALLINT NOT NULL,
+	week SMALLINT NOT NULL,
+	month SMALLINT NOT NULL,
+	year SMALLINT NOT NULL,
+	weekday SMALLINT NOT NULL
 );
